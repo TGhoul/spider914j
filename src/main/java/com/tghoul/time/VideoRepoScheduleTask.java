8e732989ca8433ba6e57691eb1b5990ef3ca360f
bootking <jetchou@outlook.com>
2017-12-11 14:42:45 +0800

    添加定时器，定时爬取链接

---
+++ src/main/java/com/tghoul/time/VideoRepoScheduleTask.java
@@ -0,0 +1,25 @@
+package com.tghoul.time;
+
+import com.tghoul.domain.S91RepoPageProcessor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import us.codecraft.webmagic.scheduler.FileCacheQueueScheduler;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zpj
+ * @date 2017/12/11 11:13
+ */
+@Component
+public class VideoRepoScheduleTask {
+    /** 爬虫主进程 */
+    @Resource
+    private S91RepoPageProcessor s91RepoPageProcessor;
+
+    /** 定时爬取，持久化数据 */
+    @Scheduled()
+    public void doCrawl() {
+        s91RepoPageProcessor.crawl();
+    }
+}
